9601e8d97d446121b4664fa720e1e76feef8e061
Nicolas Espindola <92181835+Nickfln@users.noreply.github.com>
2022-08-09 20:16:06 -0300

    Add files via upload

---
+++ desafio-dio-banco/src/ContaPoupanca.java
@@ -0,0 +1,9 @@
+
+public class ContaPoupanca extends Conta {
+	
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupanca ===");
+		super.imprimirInfosComuns();
+	}
+}
